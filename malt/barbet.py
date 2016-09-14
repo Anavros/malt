4bc5a91c15af0212088cb271f601fce6dbd006de
John Dobbs <jtdobbs1@me.com>
2016-09-14 10:07:38 -0400

    Fancified bar interface with unicode blocks.

--- malt/barbet.py
+++ malt/barbet.py
@@ -5,12 +5,14 @@ from itertools import zip_longest
 term = blessings.Terminal()
 
 SIDEBAR_X_OFFSET = 0.5
+HOR_BAR = '█'
+VER_BAR = '┇'
+PROMPT = "> "
 
-header = "HEADER"
-footer = "FOOTER"
-sidebar = "SIDEBAR\nMULTI_LINE\nOH YEAH"
-prompt = "> "
-messages = ["help"]
+header = ""
+footer = ""
+sidebar = ""
+messages = [""]
 
 def message(text):
     messages.insert(0, clean(text))
@@ -43,21 +45,15 @@ def clean(item):
         return str(item)
 
 
-def listify(string):
-    lines = string.split('\n')
-    lines = [s.strip(' \n') for s in lines]
-    if not lines[0]:
-        lines.pop(0)
-    if not lines[-1]:
-        lines.pop()
-    return lines
+def listify(string, w=0):
+    return [" {:<{w}} ".format(s.strip(' \n'), w=w) for s in string.split('\n')]
 
 
 def render():
     print(term.clear)
 
-    head_lines = listify(header)
-    foot_lines = listify(footer)
+    head_lines = listify(header, w=term.width-4)
+    foot_lines = listify(footer, w=term.width-4)
     side_lines = listify(sidebar)
     offsets = {
         'head': 2+len(head_lines),
@@ -67,24 +63,22 @@ def render():
 
     if header:
         print(term.move_y(1))
-        print(term.bold_yellow('#'*term.width))
+        print(term.bold_yellow(HOR_BAR*term.width))
         for hl in head_lines:
-            print(term.bold_yellow(term.move_x(term.width-2)+' :'), end='')
-            print(term.bold_yellow(term.move_x(0)+': '), end='')
-            print(hl)
-        print(term.bold_yellow('#'*term.width))
+            sides()
+            print(term.bold_white_on_black(hl))
+        print(term.bold_yellow(HOR_BAR*term.width))
 
     if footer:
         foot_lines.reverse()
         print(term.move_y(term.height-1))
-        print(term.bold_yellow('#'*term.width))
+        print(term.bold_yellow(HOR_BAR*term.width))
         for fl in foot_lines:
             print(term.move_up*3)
-            print(term.bold_yellow(term.move_x(term.width-2)+' :'), end='')
-            print(term.bold_yellow(term.move_x(0)+': '), end='')
-            print(fl.strip())
+            sides()
+            print(term.bold_white_on_black(fl))
         print(term.move_up*3)
-        print(term.bold_yellow('#'*term.width))
+        print(term.bold_yellow(HOR_BAR*term.width))
         #print(term.move_up*3)
         #print("Here's a popup!")
 
@@ -93,11 +87,17 @@ def render():
         fill_lines = range(offsets['head'], term.height-offsets['head']-offsets['foot'])
         print(term.move_y(offsets['head']))
         for text, i in zip_longest(side_lines, fill_lines, fillvalue=""):
-            print(term.move_x(x_off), term.bold_yellow(': '), text)
+            print(term.move_x(x_off), term.bold_yellow(VER_BAR),
+                term.bold_white(text))
 
     if messages:
         print(term.move_y(term.height-offsets['foot']-2))
         for s in messages:
-            print('> '+s.strip()+term.move_up*3)
+            print(PROMPT+s.strip()+term.move_up*3)
 
     print(term.move_y(term.height-2))
+
+
+def sides():
+    print(term.bold_yellow(term.move_x(term.width-1)+VER_BAR), end='')
+    print(term.bold_yellow(term.move_x(0)+VER_BAR), end='')
