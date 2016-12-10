b3acbd792c6871d10e0469ff0e4ad12d53e8de43
Anavros <jtdobbs1@me.com>
2016-12-10 11:07:46 -0500

    Added stack object to track [bracket enclosure].

--- malt/parser/new.py
+++ malt/parser/new.py
@@ -4,31 +4,64 @@ from malt.exceptions import MaltSyntaxError
 from malt.objects import Argument, Signature
 
 
+# Steps:
+# "stream 3 [words and things] key=value xs=[]"
+# ["stream", "3", "[words and things]", "key=value", "xs=[]"]
+# [(None, "stream"), (None, "3"), (None, "[words and things]"),
+# ("key", "value"), ("xs", "[]")]
+# ["stream", 3, ["words", "and", "things"], ("key", "value"), ("xs", [])]
+
+# Stack-Based Parsing
+# hello [is 'it really' me] 'youre looking for?'
+
+
 def parse(text):
     return Signature()
 
 
 class Token:
-    def __init__(self, key, value):
-        self.key = key
-        self.val = val
+    def __init__(self, string):
+        if EQUALS in string:
+            self.key, self.val = string.split(EQUALS, 1)
+        else:
+            self.key, self.val = None, string
 
 
-class Buffer:
+class Stack:
     def __init__(self):
-        pass
+        self.chars = []
 
+    def enclosed(self):
+        """
+        The stack is considered enclosed if there is at least one unresolved
+        opening bracket or quote.
+        """
+        return len(self.chars) > 0
 
-class ParsingState:
-    def __init__(self):
-        self.in_list = False
-        self.in_dict = False
-        self.in_single_quotes = False
-        self.in_double_quotes = False
+    def push(self, char):
+        """
+        Add a char to the stack. If it matches the previously pushed char,
+        cancel the two out, removing the enclosure.
+        """
+        if self._match(char):
+            self.chars.pop()
+        else:
+            self.chars.append(char)
 
-    def begin_list(self):
-        if self.in_list: raise MaltSyntaxError("nested list")
-        else: self.in_list = True
+    def _match(self, c):
+        """
+        Does the last character in the stack syntactically match the new one?
+        Example: [ + ], ' + ', { + }
+        """
+        if not self.chars:
+            return False
+        prev = self.chars[-1]
+        if prev == LIST_BEGIN:
+            return c == LIST_END
+        elif prev == DICT_BEGIN:
+            return c == DICT_END
+        elif prev in QUOTES:
+            return c == prev
 
 
 def tokenize(line):
@@ -36,22 +69,29 @@ def tokenize(line):
     Separate one line into string tokens.
     """
     tokens = []
-    state = ParsingState()
-    word_buffer = ""
-    list_buffer = []
-    dict_buffer = {}
+    buffer = ""
+    bracestack = []
     for c in line:
         if c in SEPARATORS:
-            pass
+            if not bracestack:
+                tokens.append(buffer)
+                buffer = ""
+            else:
+                buffer.append(c)
+
         elif c in QUOTES:
+            # ['] + ' -> []
+            # ["]
             pass
+
         elif c in BRACES:
-            if   c == LIST_BEGIN: state.begin_list()
-            elif c == LIST_END: state.end_list()
-            elif c == DICT_BEGIN: state.begin_dict()
-            elif c == DICT_END: state.end_dict()
+            if   c == LIST_BEGIN: pass
+            elif c == LIST_END: pass
+            elif c == DICT_BEGIN: pass
+            elif c == DICT_END: pass
+
         else:
-            word_buffer.append(c)
+            buffer.append(c)
     return tokens
 
 
@@ -66,12 +106,6 @@ def convert(tokens):
 
     body = []
     for i, token in enumerate(tokens):
-        # ex: 'key=value'
-        # What about turning lists into lists and etc?
-        if EQUALS in token:
-            key, value = token.split(EQUALS, 1)
-        else:
-            key, value = None, token
-
-        body.append(Argument(i, key, value, None))
+        body.append(Argument(i, token.key, token.val, None))
+    #[Argument(i, t.key, t.val, None) for i, t in enumerate(tokens)]
     return Signature(head, body)
--- tests/new_synparser_tests.py
+++ tests/new_synparser_tests.py
@@ -1,5 +1,24 @@
 
 from malt.parser.new import tokenize
+from malt.parser.new import Stack
+
+
+def test_stack_enclose():
+    stack = Stack()
+    stack.push('[')
+    assert stack.enclosed()
+    stack.push(']')
+    assert not stack.enclosed()
+
+
+def test_nested_enclosure():
+    stack = Stack()
+    stack.push('[')
+    assert stack.enclosed()
+    stack.push('\"')
+    stack.push(']')
+    assert stack.enclosed()
+    stack.push('\"')
 
 
 def hide_test_new():
